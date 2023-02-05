2c8b3dd69bf20543d9b3dfd227e68ec2f7f97d0b
Lertos <deetasticgaming@gmail.com>
2023-02-05 14:19:21 -0400

    Fixing up character info section. Adding spannables for text colorization

--- app/src/main/java/com/lertos/projectyorkie/HomePage.java
+++ app/src/main/java/com/lertos/projectyorkie/HomePage.java
@@ -1,7 +1,12 @@
 package com.lertos.projectyorkie;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,12 @@ import java.util.ArrayList;
 public class HomePage extends AppCompatActivity {
 
     private ArrayList<Talent> talentList;
+    //TODO: Make a class to hold all of this player data
+    private int highlightColor = Color.YELLOW;
+    private int currentHearts = 450;
+    private int currentHeartTokens = 14;
+    private int dogsCollected = 12;
+    private int maxDogsToCollect = 27;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,7 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.home_page);
         setupBottomButtonBar();
 
+        setupCharacterInfo();
         createAndLoadTalents();
     }
 
@@ -42,6 +54,43 @@ public class HomePage extends AppCompatActivity {
             Toast.makeText(v.getContext(), "Switching to activities page", Toast.LENGTH_SHORT).show();
             switchActivities(ActivityPage.class);
         });
+    }
+
+    private void setupCharacterInfo() {
+        TextView currentHearts = findViewById(R.id.tcCurrentHearts);
+        TextView currentHeartTokens = findViewById(R.id.tcCurrentHeartTokens);
+        TextView currentDogsCollected = findViewById(R.id.tcCurrentDogsCollected);
+
+        currentHearts.setText(
+                createSpannable(
+                        getResources().getString(R.string.character_heart_amount),
+                        " " + String.valueOf(this.currentHearts),
+                        highlightColor
+                ),
+                TextView.BufferType.SPANNABLE);
+
+        currentHeartTokens.setText(
+                createSpannable(
+                        getResources().getString(R.string.character_heart_token_amount),
+                        " " + String.valueOf(this.currentHeartTokens),
+                        highlightColor
+                ),
+                TextView.BufferType.SPANNABLE);
+
+        currentDogsCollected.setText(
+                createSpannable(
+                        getResources().getString(R.string.character_dogs_collected),
+                        " " + String.valueOf(this.dogsCollected) + " / " + String.valueOf(this.maxDogsToCollect),
+                        highlightColor
+                ),
+                TextView.BufferType.SPANNABLE);
+    }
+
+    private SpannableStringBuilder createSpannable(String str, String appended, int color) {
+        SpannableStringBuilder spannable = new SpannableStringBuilder(str + appended);
+        spannable.setSpan(new ForegroundColorSpan(color), str.length(), str.length() + appended.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        return spannable;
     }
 
     private void createAndLoadTalents() {
