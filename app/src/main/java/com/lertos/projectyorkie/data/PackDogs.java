37e2b6efab7357ee19f3048e17d37a90659131ca
Lertos <deetasticgaming@gmail.com>
2023-02-06 09:43:00 -0400

    Creating a class to hold and create all PackDog objects

---
+++ app/src/main/java/com/lertos/projectyorkie/data/PackDogs.java
@@ -0,0 +1,45 @@
+package com.lertos.projectyorkie.data;
+
+import com.lertos.projectyorkie.R;
+import com.lertos.projectyorkie.model.PackDog;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PackDogs {
+
+    List<PackDog> listPackDogs;
+
+    public PackDogs() {
+        this.listPackDogs = new ArrayList<>();
+
+        listPackDogs.add(
+                new PackDog(
+                        "Border Collie",
+                        "",
+                        R.mipmap.pack_border_collie
+                )
+        );
+
+        listPackDogs.add(
+                new PackDog(
+                        "Boxer",
+                        "",
+                        R.mipmap.pack_boxer
+                )
+        );
+
+        listPackDogs.add(
+                new PackDog(
+                        "Bull Terrier",
+                        "",
+                        R.mipmap.pack_bull_terrier
+                )
+        );
+    }
+
+    public List<PackDog> getListPackDogs() {
+        return Collections.unmodifiableList(listPackDogs);
+    }
+}
