f915f16907467d1c4f8e376a8fb9f21ff602b647
Lertos <deetasticgaming@gmail.com>
2023-02-06 10:33:34 -0400

    Adding talent list

--- app/src/main/java/com/lertos/projectyorkie/data/Data.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Data.java
@@ -1,13 +1,16 @@
 package com.lertos.projectyorkie.data;
 
 import com.lertos.projectyorkie.model.PackDog;
+import com.lertos.projectyorkie.model.Talent;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Data {
 
     private static Data instance;
+    private List<Talent> talentList = new ArrayList<>();
     private List<PackDog> packDogList = new ArrayList<>();
 
     private Data() {}
@@ -23,9 +26,16 @@ public class Data {
     public void start() {
         PackDogs packDogs = new PackDogs();
         packDogList = packDogs.getListPackDogs();
+
+        Talents talents = new Talents();
+        talentList = talents.getListTalents();
     }
 
     public List<PackDog> getPackDogs() {
-        return packDogList;
+        return Collections.unmodifiableList(packDogList);
+    }
+
+    public List<Talent> getTalents() {
+        return Collections.unmodifiableList(talentList);
     }
 }
