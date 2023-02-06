0ea8a3a2415a1fbb729474105821eabde90c2080
Lertos <deetasticgaming@gmail.com>
2023-02-06 18:42:21 -0400

    Adding sorting so the unlocked are always at the top; then sorted alphabetically

--- app/src/main/java/com/lertos/projectyorkie/data/PackDogs.java
+++ app/src/main/java/com/lertos/projectyorkie/data/PackDogs.java
@@ -5,6 +5,7 @@ import com.lertos.projectyorkie.model.PackDog;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class PackDogs {
@@ -23,6 +24,25 @@ public class PackDogs {
     }
 
     public List<PackDog> getListPackDogs() {
+        listPackDogs.sort(new SortByName());
         return Collections.unmodifiableList(listPackDogs);
+    }
+
+    class SortByName implements Comparator<PackDog> {
+        public int compare(PackDog o1, PackDog o2) {
+            boolean b1 = o1.isUnlocked();
+            boolean b2 = o2.isUnlocked();
+            int bCompare = Boolean.compare(b1, b2);
+
+            //Want to show unlocked first
+            if (bCompare != 0)
+                return -bCompare;
+
+            //If both are the same boolean value, sort by name
+            String s1 = o1.getName();
+            String s2 = o2.getName();
+
+            return s1.compareTo(s2);
+        }
     }
 }
