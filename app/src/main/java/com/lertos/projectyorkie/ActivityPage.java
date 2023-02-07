a5c13ae3a9a4b0a4687d9ab3c2d60432a47fc1ab
Lertos <deetasticgaming@gmail.com>
2023-02-06 22:06:01 -0400

    Continually updating hearts. Setting up activities page

--- app/src/main/java/com/lertos/projectyorkie/ActivityPage.java
+++ app/src/main/java/com/lertos/projectyorkie/ActivityPage.java
@@ -1,13 +1,39 @@
 package com.lertos.projectyorkie;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.lertos.projectyorkie.data.DataManager;
 
 public class ActivityPage extends HomePage {
+
+    TextView activityCurrentHearts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activities_page);
-        super.setupBottomButtonBar();
+
+        activityCurrentHearts = findViewById(R.id.activityCurrentHearts);
+
+        setupBottomButtonBar();
+        continuouslyUpdateHearts();
+    }
+
+    private void continuouslyUpdateHearts() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int currentHearts = DataManager.getInstance().getPlayerData().getCurrentHearts();
+
+                activityCurrentHearts.setText(String.valueOf(currentHearts));
+                DataManager.getInstance().getPlayerData().setCurrentHearts(currentHearts + 10);
+
+                handler.postDelayed(this, 500);
+            }
+        };
+        handler.post(runnable);
     }
 }
