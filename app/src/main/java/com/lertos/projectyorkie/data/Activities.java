a1340f687e8d35dc25a80cdba0de585b9ffb206a
Lertos <deetasticgaming@gmail.com>
2023-02-07 21:37:25 -0400

    Changing methods and example data

--- app/src/main/java/com/lertos/projectyorkie/data/Activities.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Activities.java
@@ -13,12 +13,12 @@ public class Activities {
     public Activities() {
         this.listActivities = new ArrayList<>();
 
-        listActivities.add(new Activity("Pet Their Head", 4, 1.07, 1.67));
-        listActivities.add(new Activity("Throw The Ball Around", 18, 1.065, 15));
-        listActivities.add(new Activity("Go For A Walk", 34, 1.06, 32));
+        listActivities.add(new Activity("Pet Their Head", 5, 1.08, 6));
+        listActivities.add(new Activity("Throw The Ball Around", 125, 1.075, 130));
+        listActivities.add(new Activity("Go For A Walk", 890, 1.07, 900));
 
         //TODO: Read from the file - or update the list after reading the player data "dogs_unlocked" section
-        listActivities.get(0).setCurrentLevel(1);
+        listActivities.get(0).levelUp();
         listActivities.get(0).setUnlocked(true);
     }
 
