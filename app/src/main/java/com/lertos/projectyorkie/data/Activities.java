0545986842edd66e3dd5b4a8201f2519b2cacdb6
Lertos <deetasticgaming@gmail.com>
2023-02-11 10:39:54 -0400

    Changing values; removing old logic for leveling up

--- app/src/main/java/com/lertos/projectyorkie/data/Activities.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Activities.java
@@ -13,13 +13,12 @@ public class Activities {
     public Activities() {
         this.listActivities = new ArrayList<>();
 
-        listActivities.add(new Activity("Pet Their Head", 5, 1.08, 6));
-        listActivities.add(new Activity("Throw The Ball Around", 125, 1.075, 130));
-        listActivities.add(new Activity("Go For A Walk", 890, 1.07, 900));
+        listActivities.add(new Activity("Pet Their Head", 8, 1.19, 4, 1.05));
+        listActivities.add(new Activity("Throw The Ball Around", 80, 1.18, 26, 1.06));
+        listActivities.add(new Activity("Go For A Walk", 800, 1.17, 305, 1.07));
 
         //TODO: Read from the file - or update the list after reading the player data "dogs_unlocked" section
         listActivities.get(0).levelUp();
-        listActivities.get(0).setUnlocked(true);
     }
 
     public List<Activity> getListActivities() {
