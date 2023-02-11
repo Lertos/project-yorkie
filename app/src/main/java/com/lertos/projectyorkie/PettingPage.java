c68e94865de1ebee59ced393cd25b31c805ef7fb
Lertos <deetasticgaming@gmail.com>
2023-02-10 20:31:38 -0400

    Adding the randomly placed focus button inside the layout

--- app/src/main/java/com/lertos/projectyorkie/PettingPage.java
+++ app/src/main/java/com/lertos/projectyorkie/PettingPage.java
@@ -1,26 +1,64 @@
 package com.lertos.projectyorkie;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.slider.Slider;
-import com.lertos.projectyorkie.data.MediaManager;
+import java.util.Random;
 
 public class PettingPage extends AppCompatActivity {
+
+    private Rect pettingLayout = new Rect();
+    private ImageButton focusButton;
+    private int xStart, yStart, xEnd, yEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_petting);
 
-        Helper.setupBottomButtonBar(this);
+        focusButton = findViewById(R.id.btnPettingFocus);
 
+        LinearLayout layout = (LinearLayout)findViewById(R.id.linPettingMainSection);
+        ViewTreeObserver vto = layout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                layout.getGlobalVisibleRect(pettingLayout);
+
+                int buttonHeight = focusButton.getMaxHeight();
+                int layoutMargin = 60;
+
+                xStart = pettingLayout.left;
+                yStart = pettingLayout.top  - layoutMargin;
+                xEnd = pettingLayout.right - buttonHeight;
+                yEnd = pettingLayout.bottom  - layoutMargin - buttonHeight;
+            }
+        });
+
+        Helper.setupBottomButtonBar(this);
         setOnClickListeners();
     }
 
     private void setOnClickListeners() {
+        ((Button) findViewById(R.id.btnStartPetting)).setOnClickListener( v -> {
+            Random rng = new Random();
+            int xPos = rng.nextInt(xEnd - xStart) + xStart;
+            int yPos = rng.nextInt(yEnd - yStart) + yStart;
 
+            //TODO: This needs to be so the start button begins the round and each click randomly moves the button
+            focusButton.setX(xPos);
+            focusButton.setY(yPos);
+            focusButton.setVisibility(View.VISIBLE);
+        });
     }
 
 }
+
