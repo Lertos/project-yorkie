a0c04b48a08994b2826c1d4a6c64d81792d40293
Lertos <deetasticgaming@gmail.com>
2023-02-13 22:17:37 -0400

    Adding the positions to the Activities

--- app/src/main/java/com/lertos/projectyorkie/data/Activities.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Activities.java
@@ -13,9 +13,9 @@ public class Activities {
     public Activities() {
         this.listActivities = new ArrayList<>();
 
-        listActivities.add(new Activity("Pet Their Head", 8, 1.19, 4, 1.05));
-        listActivities.add(new Activity("Throw The Ball Around", 80, 1.18, 26, 1.06));
-        listActivities.add(new Activity("Go For A Walk", 800, 1.17, 305, 1.07));
+        listActivities.add(new Activity("Pet Their Head", 1));
+        listActivities.add(new Activity("Throw The Ball Around", 2));
+        listActivities.add(new Activity("Go For A Walk", 3));
 
         //TODO: Read from the file - or update the list after reading the player data "dogs_unlocked" section
         listActivities.get(0).levelUp();
