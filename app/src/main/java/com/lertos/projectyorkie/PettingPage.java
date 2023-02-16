c73c3ea211ceb462aaa8001eaf5f85e3e16030e0
Lertos <deetasticgaming@gmail.com>
2023-02-16 18:33:08 -0400

    Adding main UI update loop; adding logic to execute before each round

--- app/src/main/java/com/lertos/projectyorkie/PettingPage.java
+++ app/src/main/java/com/lertos/projectyorkie/PettingPage.java
@@ -2,6 +2,7 @@ package com.lertos.projectyorkie;
 
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -17,9 +18,14 @@ import java.util.Random;
 
 public class PettingPage extends AppCompatActivity {
 
+    private PettingMaster pettingMaster;
     private Rect pettingLayout = new Rect();
     private ImageButton focusButton;
+    private LinearProgressIndicator indicator;
+    private TextView timerInSeconds;
+    private double timerStartValue;
     private int xStart, yStart, xEnd, yEnd;
+    private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class PettingPage extends AppCompatActivity {
         setContentView(R.layout.page_petting);
 
         focusButton = findViewById(R.id.btnPettingFocus);
+        indicator = findViewById(R.id.indPettingTimer);
+        timerInSeconds = findViewById(R.id.tvPettingTimerInSecs);
 
         LinearLayout layout = (LinearLayout)findViewById(R.id.linPettingMainSection);
         ViewTreeObserver vto = layout.getViewTreeObserver();
@@ -63,11 +71,17 @@ public class PettingPage extends AppCompatActivity {
             //Show the timer
             findViewById(R.id.linPettingTimerSection).setVisibility(View.VISIBLE);
 
-            //TODO: Remove - just for testing purposes
-            LinearProgressIndicator indicator = findViewById(R.id.indPettingTimer);
-            indicator.setProgressCompat(20, false);
+            //Create a new instance of the petting mini game master
+            //TODO: Use actual values instead of test values
+            pettingMaster = new PettingMaster(1, 5, 1, 1);
+            timerStartValue = pettingMaster.getTimerStartValue();
+            isPlaying = true;
 
-            ((TextView) findViewById(R.id.tvPettingTimerInSecs)).setText("30.26s");
+            indicator.setProgressCompat(100, false);
+            updateIndicatorWithTime();
+
+            //Start the mini game
+            pettingMaster.start();
 
             //Make the generated square visible after setup is done
             focusButton.setVisibility(View.VISIBLE);
@@ -76,6 +90,33 @@ public class PettingPage extends AppCompatActivity {
         ((ImageButton) findViewById(R.id.btnPettingFocus)).setOnClickListener( v -> {
             moveClickSquare();
         });
+    }
+
+    private void updateIndicatorWithTime() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                double currentTimeLeft = pettingMaster.getCurrentTimeLeft();
+                int calculatedProgress = (int) Math.round(currentTimeLeft / timerStartValue);
+
+                indicator.setProgress(calculatedProgress, false);
+                timerInSeconds.setText(String.valueOf(currentTimeLeft));
+
+                if (currentTimeLeft <= 0)
+                    isPlaying = false;
+
+                if(!isPlaying)
+                    handler.removeCallbacks(this);
+                else
+                    handler.postDelayed(this, pettingMaster.getMillisecondsPerUpdate());
+            }
+        };
+        handler.post(runnable);
+    }
+
+    private double getIndicatorValueOfTimeLeft(double currentTimeLeft) {
+        return Helper.roundNumber(currentTimeLeft / timerStartValue);
     }
 
     private void moveClickSquare() {
