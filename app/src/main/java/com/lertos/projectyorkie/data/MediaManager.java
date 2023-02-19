4f0f4c22d86625fb4957879cd66b6ef5f82e9c18
Lertos <deetasticgaming@gmail.com>
2023-02-19 13:15:41 -0400

    Fixing random error in mediaPlayer; wasn't releasing or setting to NULL

--- app/src/main/java/com/lertos/projectyorkie/data/MediaManager.java
+++ app/src/main/java/com/lertos/projectyorkie/data/MediaManager.java
@@ -24,27 +24,47 @@ public class MediaManager {
 
     public void start(Context context) {
         this.context = context;
-        this.trackEffect = new MediaPlayer();
-        this.trackSong = new MediaPlayer();
         //TODO: Have these saved somewhere in the user prefs
         this.trackEffectVolume = 0.5f;
         this.trackSongVolume = 0.0f;
     }
 
-    public void playEffectTrack(int resId) {
+    private void resetEffectTrack() {
         if (trackEffect.isPlaying())
             trackEffect.stop();
 
+        trackEffect.release();
+        trackEffect = null;
+    }
+
+    private void resetSongTrack() {
+        if (trackSong.isPlaying())
+            trackSong.stop();
+
+        trackSong.release();
+        trackSong = null;
+    }
+
+    public void playEffectTrack(int resId) {
+        if (trackEffect != null)
+            resetEffectTrack();
+
         trackEffect = MediaPlayer.create(this.context, resId);
+
+        trackEffect.setOnCompletionListener(player -> resetEffectTrack());
+
         trackEffect.setVolume(trackEffectVolume, trackEffectVolume);
         trackEffect.start();
     }
 
     public void playSongTrack(int resId, boolean loopSong) {
-        if (trackSong.isPlaying())
-            trackSong.stop();
+        if (trackSong != null)
+            resetSongTrack();
 
         trackSong = MediaPlayer.create(this.context, resId);
+
+        trackEffect.setOnCompletionListener(player -> resetSongTrack());
+
         trackSong.setVolume(trackSongVolume, trackSongVolume);
         trackSong.start();
 
