afa2a2a0b9a191d6a03f5e4a09e87f4499561efe
Lertos <deetasticgaming@gmail.com>
2023-02-20 21:59:32 -0400

    Adding a way to get the string display value of each type

--- app/src/main/java/com/lertos/projectyorkie/model/TalentBonusType.java
+++ app/src/main/java/com/lertos/projectyorkie/model/TalentBonusType.java
@@ -2,5 +2,16 @@ package com.lertos.projectyorkie.model;
 
 public enum TalentBonusType {
     FLAT,
-    PERCENTAGE
+    PERCENTAGE;
+
+    private String suffix;
+
+    static {
+        FLAT.suffix = "";
+        PERCENTAGE.suffix = "%";
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
 }
