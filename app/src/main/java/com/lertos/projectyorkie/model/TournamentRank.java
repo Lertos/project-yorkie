687f3cb61e19041926f73c9dcba6ddace234e605
Lertos <deetasticgaming@gmail.com>
2023-02-27 16:13:17 -0400

    Adding class to handle the divisions and tiers for tournaments

---
+++ app/src/main/java/com/lertos/projectyorkie/model/TournamentRank.java
@@ -0,0 +1,90 @@
+package com.lertos.projectyorkie.model;
+
+public class TournamentRank {
+
+    //Tiers go from V > IV > III > II > I for example,
+    private final int startTier = 5;
+    private final int maxTier = 1;
+    private TournamentDivision division;
+    private int tier;
+
+    public TournamentRank(TournamentDivision division, int tier) {
+        this.division = division;
+        this.tier = tier;
+    }
+
+    public TournamentDivision getDivision() {
+        return division;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    //Returns true if they move up a division, false otherwise
+    public boolean increaseTier() {
+        tier--;
+
+        if (tier < maxTier) {
+            TournamentDivision newDivision = division.getNextDivision();
+
+            //If they are at the max division - there is nothing to do; reset to default
+            if (newDivision == null) {
+                tier = maxTier;
+                return false;
+            }
+            //If they are not at max division
+            else {
+                division = newDivision;
+                tier = startTier;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Returns true if they move down a division, false otherwise
+    public boolean decreaseTier() {
+        tier++;
+
+        if (tier > startTier) {
+            TournamentDivision newDivision = division.getPreviousDivision();
+
+            //If they are at the bottom division - there is nothing to do; reset to default
+            if (newDivision == null) {
+                tier = startTier;
+                return false;
+            }
+            //If they are not at the bottom division
+            else {
+                division = newDivision;
+                tier = maxTier;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getRankDisplay() {
+        StringBuilder sb = new StringBuilder(division.getDisplayStr());
+        sb.append(" ");
+        sb.append(getRomanNumeral(tier));
+
+        return sb.toString();
+    }
+
+    private String getRomanNumeral(int number) {
+        if (number == 1)
+            return "I";
+        else if (number == 2)
+            return "II";
+        else if (number == 3)
+            return "III";
+        else if (number == 4)
+            return "IV";
+        else if (number == 5)
+            return "V";
+        return "NaN";
+    }
+
+}
