a7e1bc832bad084dfb3a7993f90f9e154906b0ae
Lertos <deetasticgaming@gmail.com>
2023-02-28 17:54:03 -0400

    Adding beginning of contestant logic

--- app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
+++ app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
@@ -1,5 +1,15 @@
 package com.lertos.projectyorkie.tournament;
 
+import java.util.ArrayList;
+
 public class TournamentMaster {
+
+    private ArrayList<TournamentContestant> contestants;
+
+
+    private void createContestants() {
+        //Fill the contestant list
+        //Add the player first, then fill the rest with unique PackDogs - checking that it doesn't already exist in the list
+    }
 
 }
