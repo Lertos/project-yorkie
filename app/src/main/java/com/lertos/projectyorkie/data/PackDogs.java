4e51f00f9175391fd011b3e220961a8d4465def0
Lertos <deetasticgaming@gmail.com>
2023-03-01 10:39:59 -0400

    Adding rest of pack dogs

--- app/src/main/java/com/lertos/projectyorkie/data/PackDogs.java
+++ app/src/main/java/com/lertos/projectyorkie/data/PackDogs.java
@@ -18,6 +18,30 @@ public class PackDogs {
         listPackDogs.add(new PackDog("Border Collie", R.mipmap.pack_border_collie));
         listPackDogs.add(new PackDog("Boxer", R.mipmap.pack_boxer));
         listPackDogs.add(new PackDog("Bull Terrier", R.mipmap.pack_bull_terrier));
+        listPackDogs.add(new PackDog("Bulldog", R.mipmap.pack_bulldog));
+        listPackDogs.add(new PackDog("King Charles Spaniel", R.mipmap.pack_cavalier_king_charles_spaniel));
+        listPackDogs.add(new PackDog("Chihuahua", R.mipmap.pack_chihuahua));
+        listPackDogs.add(new PackDog("Cocker Spaniel", R.mipmap.pack_cocker_spaniel));
+        listPackDogs.add(new PackDog("Dachshund", R.mipmap.pack_dachshund));
+        listPackDogs.add(new PackDog("Dalmation", R.mipmap.pack_dalmation));
+        listPackDogs.add(new PackDog("Doberman", R.mipmap.pack_doberman));
+        listPackDogs.add(new PackDog("French Bulldog", R.mipmap.pack_french_bulldog));
+        listPackDogs.add(new PackDog("German Shepherd", R.mipmap.pack_german_shepherd));
+        listPackDogs.add(new PackDog("Golden Retriever", R.mipmap.pack_golden_retriever));
+        listPackDogs.add(new PackDog("Great Dane", R.mipmap.pack_great_dane));
+        listPackDogs.add(new PackDog("Hound", R.mipmap.pack_hound));
+        listPackDogs.add(new PackDog("Jack Russell Terrier", R.mipmap.pack_jack_russell_terrier));
+        listPackDogs.add(new PackDog("Labrador Retriever", R.mipmap.pack_labrador_retriever));
+        listPackDogs.add(new PackDog("Pitbull", R.mipmap.pack_pitbull));
+        listPackDogs.add(new PackDog("Pomeranian", R.mipmap.pack_pomeranian));
+        listPackDogs.add(new PackDog("Poodle", R.mipmap.pack_poodle));
+        listPackDogs.add(new PackDog("Pug", R.mipmap.pack_pug));
+        listPackDogs.add(new PackDog("Rottweiler", R.mipmap.pack_rottweiler));
+        listPackDogs.add(new PackDog("Saint Bernard", R.mipmap.pack_saint_bernard));
+        listPackDogs.add(new PackDog("Saluki", R.mipmap.pack_saluki));
+        listPackDogs.add(new PackDog("Schnauzer", R.mipmap.pack_schnauzer));
+        listPackDogs.add(new PackDog("Shih Tzu", R.mipmap.pack_shih_tzu));
+        listPackDogs.add(new PackDog("Siberian Husky", R.mipmap.pack_siberian_husky));
 
         //TODO: Read from the file - or update the list after reading the player data "dogs_unlocked" section
         listPackDogs.get(0).setUnlocked(true);
