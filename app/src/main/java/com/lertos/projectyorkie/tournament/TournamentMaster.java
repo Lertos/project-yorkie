1be2f4cd3cb1facd76764cf51e6017c27297ab28
Lertos <deetasticgaming@gmail.com>
2023-03-09 14:25:44 -0400

    Changing name to "YOU" instead of "Yorkie" incase they dont know

--- app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
+++ app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
@@ -107,7 +107,7 @@ public class TournamentMaster {
     private ArrayList<TournamentContestant> createContestants() {
         ArrayList<TournamentContestant> contestantList = new ArrayList<>();
 
-        PackDog player = new PackDog("Yorkie", R.mipmap.portrait_loki);
+        PackDog player = new PackDog("YOU", R.mipmap.portrait_loki);
         contestantList.add(new TournamentContestant(player, true));
 
         for (int i=0; i < maxAIContestants; i++) {
