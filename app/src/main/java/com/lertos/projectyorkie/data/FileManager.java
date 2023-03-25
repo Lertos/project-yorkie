92e46448e8d587bac20129975d79294a55a17f68
Lertos <deetasticgaming@gmail.com>
2023-03-25 14:55:00 -0300

    Adding values for all data and setting fields; adding separators; removing old methods

--- app/src/main/java/com/lertos/projectyorkie/data/FileManager.java
+++ app/src/main/java/com/lertos/projectyorkie/data/FileManager.java
@@ -17,29 +17,32 @@ public class FileManager {
     private final String FILE_NAME_SETTINGS = "settings";
     private final String FILE_NAME_PLAYER_DATA = "playerData";
 
-    private final String DATA_HAS_PLAYED_BEFORE = "";
-    private final String DATA_LAST_TIME_ON = "";
-    private final String DATA_SHOW_TUT_HOME = "";
-    private final String DATA_SHOW_TUT_ACTIVITY = "";
-    private final String DATA_SHOW_TUT_PETTING = "";
-    private final String DATA_SHOW_TUT_TOURNAMENT = "";
-    private final String DATA_SHOW_TUT_CATCH_DOG_TREATS = "";
-    private final String DATA_SHOW_TUT_DODGE_THE_CATS = "";
-    private final String DATA_SHOW_TUT_TREAT_TOSS = "";
-    private final String DATA_SHOW_TUT_WHACK_THE_CAT = "";
-    private final String DATA_HIGH_SCORE_THRESHOLD = "";
-    private final String DATA_HIGH_SCORE_SQUARE = "";
-    private final String DATA_CURRENT_RANK_NAME = "";
-    private final String DATA_CURRENT_RANK_TIER = "";
-    private final String DATA_CURRENT_HEARTS = "";
-    private final String DATA_CURRENT_HEART_TOKENS = "";
-    private final String DATA_PACK_DOGS_UNLOCKED = "";
-    private final String DATA_TALENT_LEVELS = "";
-    private final String DATA_ACTIVITY_LEVELS = "";
+    private final String PAIR_SEPARATOR = ":";
+    private final String VALUE_SEPARATOR = "|";
 
-    private final String SETTING_EFFECT_VOLUME = "";
-    private final String SETTING_MUSIC_VOLUME = "";
-    private final String SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT = "";
+    private final String DATA_HAS_PLAYED_BEFORE = "DATA_HAS_PLAYED_BEFORE";
+    private final String DATA_LAST_TIME_ON = "DATA_LAST_TIME_ON";
+    private final String DATA_SHOW_TUT_HOME = "DATA_SHOW_TUT_HOME";
+    private final String DATA_SHOW_TUT_ACTIVITY = "DATA_SHOW_TUT_ACTIVITY";
+    private final String DATA_SHOW_TUT_PETTING = "DATA_SHOW_TUT_PETTING";
+    private final String DATA_SHOW_TUT_TOURNAMENT = "DATA_SHOW_TUT_TOURNAMENT";
+    private final String DATA_SHOW_TUT_CATCH_DOG_TREATS = "DATA_SHOW_TUT_CATCH_DOG_TREATS";
+    private final String DATA_SHOW_TUT_DODGE_THE_CATS = "DATA_SHOW_TUT_DODGE_THE_CATS";
+    private final String DATA_SHOW_TUT_TREAT_TOSS = "DATA_SHOW_TUT_TREAT_TOSS";
+    private final String DATA_SHOW_TUT_WHACK_THE_CAT = "DATA_SHOW_TUT_WHACK_THE_CAT";
+    private final String DATA_HIGH_SCORE_THRESHOLD = "DATA_HIGH_SCORE_THRESHOLD";
+    private final String DATA_HIGH_SCORE_SQUARE = "DATA_HIGH_SCORE_SQUARE";
+    private final String DATA_CURRENT_RANK_NAME = "DATA_CURRENT_RANK_NAME";
+    private final String DATA_CURRENT_RANK_TIER = "DATA_CURRENT_RANK_TIER";
+    private final String DATA_CURRENT_HEARTS = "DATA_CURRENT_HEARTS";
+    private final String DATA_CURRENT_HEART_TOKENS = "DATA_CURRENT_HEART_TOKENS";
+    private final String DATA_PACK_DOGS_UNLOCKED = "DATA_PACK_DOGS_UNLOCKED";
+    private final String DATA_TALENT_LEVELS = "DATA_TALENT_LEVELS";
+    private final String DATA_ACTIVITY_LEVELS = "DATA_ACTIVITY_LEVELS";
+
+    private final String SETTING_EFFECT_VOLUME = "SETTING_EFFECT_VOLUME";
+    private final String SETTING_MUSIC_VOLUME = "SETTING_MUSIC_VOLUME";
+    private final String SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT = "SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT";
 
     public FileManager(Context context) {
         this.context = context;
@@ -97,11 +100,4 @@ public class FileManager {
         }
     }
 
-    public void updateSettings(String key, String newValue) {
-
-    }
-
-    public void updatePlayerData(String key, String newValue) {
-
-    }
 }
