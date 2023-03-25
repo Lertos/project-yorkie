da227aa4b7d9805d70521164f652d634e3f4aa0b
Lertos <deetasticgaming@gmail.com>
2023-03-25 18:25:29 -0300

    Adding more methods to take care of missing defaults

--- app/src/main/java/com/lertos/projectyorkie/data/file/DataFile.java
+++ app/src/main/java/com/lertos/projectyorkie/data/file/DataFile.java
@@ -2,9 +2,14 @@ package com.lertos.projectyorkie.data.file;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import kotlin.Triple;
@@ -13,11 +18,15 @@ import kotlin.Triple;
 public class DataFile {
 
     protected List<Triple> listOfDataKeys;
-    protected String fileName;
+    protected final String fileName;
+    protected final String PAIR_SEPARATOR = ":";
     protected Context context;
+    protected final String VALUE_SEPARATOR = "|";
+    protected List<String> existingKeys;
 
     public DataFile(List<Triple> listOfDataKeys, String fileName, Context context) {
         this.listOfDataKeys = listOfDataKeys;
+        this.existingKeys = new ArrayList<>();
         this.fileName = fileName;
         this.context = context;
 
@@ -28,6 +37,7 @@ public class DataFile {
         if (!fileExists(fileName))
             createFile();
 
+        getExistingKeys();
         addMissingDefaults();
     }
 
@@ -51,8 +61,57 @@ public class DataFile {
         return false;
     }
 
-    private void addMissingDefaults() {
+    private void getExistingKeys() {
+        FileInputStream fis;
 
+        try {
+            fis = context.openFileInput(fileName);
+        } catch (FileNotFoundException e) {
+            return;
+        }
+
+        InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                existingKeys.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    private void addMissingDefaults() {
+        List<Triple> keysToAdd = new ArrayList<>();
+
+        //Find all missing keys
+        for (Triple triple : listOfDataKeys) {
+            String key = triple.getFirst().toString();
+
+            //If the key does not exist, add it and
+            if (!existingKeys.contains(key))
+                keysToAdd.add(triple);
+        }
+
+        writeMissingKeysToFile(keysToAdd);
+    }
+
+    private void writeMissingKeysToFile(List<Triple> keysToAdd) {
+        try (FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE + Context.MODE_APPEND)) {
+            StringBuilder sb = new StringBuilder();
+
+            for (Triple triple : keysToAdd)
+                sb.append(triple.getFirst()).append(PAIR_SEPARATOR).append(triple.getThird());
+
+            fos.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
