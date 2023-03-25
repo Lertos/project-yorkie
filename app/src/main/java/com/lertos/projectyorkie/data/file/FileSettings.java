c985829b7c35a8d43f3b13daeb6c5c15fdb00f46
Lertos <deetasticgaming@gmail.com>
2023-03-25 18:57:58 -0300

    Creating a child class for the settings file

---
+++ app/src/main/java/com/lertos/projectyorkie/data/file/FileSettings.java
@@ -0,0 +1,28 @@
+package com.lertos.projectyorkie.data.file;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import kotlin.Triple;
+
+
+public class FileSettings extends DataFile {
+
+    private ArrayList<Triple> defaultKeys = new ArrayList<>(Arrays.asList(
+            new Triple("test", boolean.class, "0")
+    ));
+
+    public FileSettings(Context context) {
+        super("settings", context);
+
+        setListOfDataKeys();
+        setupFile();
+    }
+
+    @Override
+    protected void setListOfDataKeys() {
+        listOfDataKeys = (ArrayList<Triple>) defaultKeys.clone();
+    }
+}
