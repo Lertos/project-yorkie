3990873ec45e8e19886ee16c74d13d5d61291563
Lertos <deetasticgaming@gmail.com>
2023-03-26 11:04:03 -0300

    Adding the keys as an enum to use when looking for a specific value

---
+++ app/src/main/java/com/lertos/projectyorkie/data/file/FileSettingsKeys.java
@@ -0,0 +1,9 @@
+package com.lertos.projectyorkie.data.file;
+
+public enum FileSettingsKeys {
+
+    SETTING_EFFECT_VOLUME,
+    SETTING_MUSIC_VOLUME,
+    SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT
+
+}
