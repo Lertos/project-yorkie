c8d95638b758c42f3e5455f6530dacacd1659459
Lertos <deetasticgaming@gmail.com>
2023-03-26 15:00:39 -0300

    Changing boolean string value for Boolean.getValueOf method

--- app/src/main/java/com/lertos/projectyorkie/data/file/FileSettings.java
+++ app/src/main/java/com/lertos/projectyorkie/data/file/FileSettings.java
@@ -13,7 +13,7 @@ public class FileSettings extends DataFile {
     private ArrayList<Triple> defaultKeys = new ArrayList<>(Arrays.asList(
             new Triple(FileSettingsKeys.SETTING_EFFECT_VOLUME, float.class, "0.5"),
             new Triple(FileSettingsKeys.SETTING_MUSIC_VOLUME, float.class, "0.25"),
-            new Triple(FileSettingsKeys.SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT, boolean.class, "1")
+            new Triple(FileSettingsKeys.SETTING_SHOW_ANIMATIONS_IN_TOURNAMENT, boolean.class, "true")
     ));
 
     public FileSettings(Context context) {
