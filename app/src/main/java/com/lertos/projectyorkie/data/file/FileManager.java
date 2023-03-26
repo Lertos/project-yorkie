1613bea8ce383ff33279993137d5251d2d86dd7a
Lertos <deetasticgaming@gmail.com>
2023-03-26 11:17:59 -0300

    Adding player data file

--- app/src/main/java/com/lertos/projectyorkie/data/file/FileManager.java
+++ app/src/main/java/com/lertos/projectyorkie/data/file/FileManager.java
@@ -6,12 +6,16 @@ import android.util.Log;
 public class FileManager {
 
     private FileSettings fileSettings;
+    private FileData fileData;
 
     public FileManager(Context context) {
         fileSettings = new FileSettings(context);
+        fileData = new FileData(context);
 
-        Log.d("===KEYS", fileSettings.getListOfDataKeys().toString());
+        Log.d("===SETTINGS KEYS", fileSettings.getListOfDataKeys().toString());
         Log.d("===VALUE of SETTING_EFFECT_VOLUME", fileSettings.getValueOfKey(FileSettingsKeys.SETTING_EFFECT_VOLUME));
 
+        Log.d("===DATA KEYS", fileData.getListOfDataKeys().toString());
+        Log.d("===VALUE of DATA_CURRENT_HEARTS", fileData.getValueOfKey(FileDataKeys.DATA_CURRENT_HEARTS));
     }
 }
