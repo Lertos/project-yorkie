7ac22a24435f094c68b9bcf12b8025ea7ff737fd
Lertos <deetasticgaming@gmail.com>
2023-03-27 17:41:24 -0300

    Adding the keys so that we know what value to update in the file

--- app/src/main/java/com/lertos/projectyorkie/data/Tutorial.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Tutorial.java
@@ -1,17 +1,21 @@
 package com.lertos.projectyorkie.data;
 
+import com.lertos.projectyorkie.data.file.FileTutorialsKeys;
+
 public class Tutorial {
 
     private final String matchingClassName;
     private final int layoutId;
     private final int viewStubId;
-    boolean hasPlayerSeen;
+    private boolean hasPlayerSeen;
+    private FileTutorialsKeys fileKey;
 
-    public Tutorial(String gameClassName, int layoutId, int viewStubId, boolean hasPlayerSeen) {
+    public Tutorial(String gameClassName, int layoutId, int viewStubId, boolean hasPlayerSeen, FileTutorialsKeys fileKey) {
         this.matchingClassName = gameClassName;
         this.layoutId = layoutId;
         this.viewStubId = viewStubId;
         this.hasPlayerSeen = hasPlayerSeen;
+        this.fileKey = fileKey;
     }
 
     public String getMatchingClassName() {
@@ -24,6 +28,7 @@ public class Tutorial {
 
     public void setHasPlayerSeen(boolean hasPlayerSeen) {
         this.hasPlayerSeen = hasPlayerSeen;
+        DataManager.getInstance().getFiles().getTutorialFile().setValue(fileKey, !hasPlayerSeen);
     }
 
     public int getLayoutId() {
