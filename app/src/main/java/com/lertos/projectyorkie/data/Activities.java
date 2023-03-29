503f6141cbacb15448c69c938c554ef7ab7a0560
Lertos <deetasticgaming@gmail.com>
2023-03-29 19:56:03 -0300

    Balancing tokens from activities

--- app/src/main/java/com/lertos/projectyorkie/data/Activities.java
+++ app/src/main/java/com/lertos/projectyorkie/data/Activities.java
@@ -15,18 +15,18 @@ public class Activities {
         this.listActivities = new ArrayList<>();
         this.separator = separator;
 
-        listActivities.add(new Activity("Pet Their Head", 2, 0.1));
-        listActivities.add(new Activity("Throw the Ball Around", 3, 0.16));
-        listActivities.add(new Activity("Go for a Walk", 4, 0.27));
-        listActivities.add(new Activity("Cover Them in a Blanket", 5, 0.39));
-        listActivities.add(new Activity("Feed Them Low-Grade Food", 6, 0.65));
-        listActivities.add(new Activity("Teach Them Tricks", 7, 0.95));
-        listActivities.add(new Activity("Rub Their Paws", 8, 1.35));
-        listActivities.add(new Activity("Feed Them Medium-Grade Food", 9, 1.9));
-        listActivities.add(new Activity("Play with the Squeaky Toy", 10, 2.5));
-        listActivities.add(new Activity("Call the Mailman Over", 11, 3.8));
-        listActivities.add(new Activity("Stroke Behind the Ears", 12, 5.3));
-        listActivities.add(new Activity("Feed Them Premium-Grade Food", 13, 8.9));
+        listActivities.add(new Activity("Pet Their Head", 2, 1.25));
+        listActivities.add(new Activity("Throw the Ball Around", 3, 0.028));
+        listActivities.add(new Activity("Go for a Walk", 4, 0.056));
+        listActivities.add(new Activity("Cover Them in a Blanket", 5, 0.084));
+        listActivities.add(new Activity("Feed Them Low-Grade Food", 6, 0.112));
+        listActivities.add(new Activity("Teach Them Tricks", 7, 0.140));
+        listActivities.add(new Activity("Rub Their Paws", 8, 0.168));
+        listActivities.add(new Activity("Feed Them Medium-Grade Food", 9, 0.196));
+        listActivities.add(new Activity("Play with the Squeaky Toy", 10, 0.224));
+        listActivities.add(new Activity("Call the Mailman Over", 11, 0.252));
+        listActivities.add(new Activity("Stroke Behind the Ears", 12, 0.281));
+        listActivities.add(new Activity("Feed Them Premium-Grade Food", 13, 0.309));
 
         listActivities.get(0).setUnlocked(true);
         listActivities.get(0).setCurrentLevel(1);
