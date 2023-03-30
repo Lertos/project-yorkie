6b9d86802701b998f9c7624de3eae3668402cbe5
Lertos <deetasticgaming@gmail.com>
2023-03-30 08:04:23 -0300

    Removing unused import

--- app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
+++ app/src/main/java/com/lertos/projectyorkie/tournament/TournamentMaster.java
@@ -16,7 +16,6 @@ import com.lertos.projectyorkie.tournament.games.TreatToss;
 import com.lertos.projectyorkie.tournament.games.WhackTheCat;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
